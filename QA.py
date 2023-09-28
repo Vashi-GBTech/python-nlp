0a06bf01f2a83ff11abf45b5d6084f925bbd3350
shwetagbtech <shweta@gbtech.in>
2023-09-28 12:28:20 +0530

    NLP QA model

---
+++ QA.py
@@ -0,0 +1,47 @@
+from datasets import load_dataset, load_metric
+from transformers import DistilBertTokenizerFast, AutoModelForSequenceClassification, DataCollatorWithPadding, Trainer, TrainingArguments
+
+# Define the checkpoint and load the dataset
+checkpoint = "distilbert-base-uncased"
+dataset = load_dataset("boolq")
+
+# Tokenizer initialization
+tokenizer = DistilBertTokenizerFast.from_pretrained(checkpoint)
+
+# Tokenization function
+def tokenize_function(example):
+    encoded = tokenizer(example["question"], example["passage"], truncation=True)
+    encoded["labels"] = [int(a) for a in example["answer"]]
+    return encoded
+
+# Tokenize the datasets and collate data
+tokenized_datasets = dataset.map(tokenize_function, batched=True)
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+# Initialize the model
+model = AutoModelForSequenceClassification.from_pretrained(checkpoint, num_labels=2)
+
+# Training setup
+args = TrainingArguments(
+    # Define training arguments
+    output_dir="./results",
+    per_device_train_batch_size=8,
+    evaluation_strategy="steps",
+    eval_steps=500,  # Set as needed
+    save_steps=500,  # Set as needed
+    num_train_epochs=3,
+    save_total_limit=2,
+)
+
+# Initialize the Trainer
+trainer = Trainer(
+    model,
+    args,
+    train_dataset=tokenized_datasets["train"],
+    eval_dataset=tokenized_datasets["validation"],
+    data_collator=data_collator,
+    tokenizer=tokenizer,
+)
+
+# Start training
+trainer.train()
---
+++ model.py
@@ -0,0 +1,34 @@
+from transformers import DistilBertTokenizer, DistilBertForQuestionAnswering
+import torch
+
+tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-cased-distilled-squad")
+model = DistilBertForQuestionAnswering.from_pretrained("distilbert-base-cased-distilled-squad")
+
+def answer_question(question, context):
+    encoding = tokenizer.encode_plus(question, context, add_special_tokens=True, return_tensors="pt")
+    input_ids = encoding["input_ids"]
+    attention_mask = encoding["attention_mask"]
+
+    start_scores, end_scores = model(input_ids, attention_mask=attention_mask)
+
+    start_index = torch.argmax(start_scores)
+    end_index = torch.argmax(end_scores)
+
+    answer_tokens = input_ids[0][start_index : end_index + 1]
+    answer = tokenizer.decode(answer_tokens)
+
+    return answer
+
+context = "DistilBERT is a lightweight version of BERT that is efficient for various NLP tasks. It has been pre-trained on large amounts of text data."
+
+question1 = "What is DistilBERT?"
+question2 = "What is the benefit of using DistilBERT?"
+question3 = "Which tasks can DistilBERT be used for?"
+
+answer1 = answer_question(question1, context)
+answer2 = answer_question(question2, context)
+answer3 = answer_question(question3, context)
+
+print(f"Q: {question1}\nA: {answer1}")
+print(f"Q: {question2}\nA: {answer2}")
+print(f"Q: {question3}\nA: {answer3}")
